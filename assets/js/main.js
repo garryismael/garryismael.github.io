cf01b50be8cbb236ebb8ac257a4dc161fcd19583
garry <tahinjanaharygarry@gmail.com>
2025-04-08 09:45:53 +1400

    add menu on mobile version

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,9 @@ AOS.init();
 const hamburger = document.querySelector(".hamburger");
 const navigation = document.querySelector("header > nav");
 const links = document.querySelectorAll(".navigations li");
-
-hamburger.addEventListener("click", () => {
-  navigation.classList.toggle("open");
-  hamburger.classList.toggle("open");
-
-  links.forEach((link) => {
-    link.classList.toggle("fade");
-  });
-});
+const mobileMenu = document.getElementById("mobileMenu");
+const closeBtn = document.getElementById("closeMenu");
+const mobileLinks = document.querySelectorAll(".mobile-link");
 
 const year = document.querySelector("#year");
 
@@ -26,17 +20,43 @@ const nextButtonNode = rootNode.querySelector(".embla__next");
 const options = { loop: false };
 const emblaApi = EmblaCarousel(viewportNode, options);
 
-prevButtonNode.addEventListener('click', emblaApi.scrollPrev, false)
-nextButtonNode.addEventListener('click', emblaApi.scrollNext, false)
-
+prevButtonNode.addEventListener("click", emblaApi.scrollPrev, false);
+nextButtonNode.addEventListener("click", emblaApi.scrollNext, false);
 
 const projectNode = document.querySelector(".embla_project");
-const projectViewportNode = projectNode.querySelector(".embla__viewport_project");
+const projectViewportNode = projectNode.querySelector(
+  ".embla__viewport_project"
+);
 const prevProjectButtonNode = projectNode.querySelector(".embla__prev_project");
 const nextProjectButtonNode = projectNode.querySelector(".embla__next_project");
 
 const projectOptions = { loop: false };
 const emblaProjectApi = EmblaCarousel(projectViewportNode, projectOptions);
 
-prevProjectButtonNode.addEventListener('click', emblaProjectApi.scrollPrev, false)
-nextProjectButtonNode.addEventListener('click', emblaProjectApi.scrollNext, false)
+prevProjectButtonNode.addEventListener(
+  "click",
+  emblaProjectApi.scrollPrev,
+  false
+);
+nextProjectButtonNode.addEventListener(
+  "click",
+  emblaProjectApi.scrollNext,
+  false
+);
+
+const openMenu = () => {
+  mobileMenu.classList.remove("translate-x-full");
+  mobileMenu.classList.add("translate-x-0");
+  document.body.classList.add("no-scroll");
+};
+
+hamburger.addEventListener("click", openMenu);
+
+const closeMenu = () => {
+  mobileMenu.classList.add("translate-x-full");
+  mobileMenu.classList.remove("translate-x-0");
+  document.body.classList.remove("no-scroll");
+};
+
+closeBtn.addEventListener("click", closeMenu);
+mobileLinks.forEach((link) => link.addEventListener("click", closeMenu));
